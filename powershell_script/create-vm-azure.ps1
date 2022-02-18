93568f9a2bde868349e82005a527d16398ce8d30
breathinglogic <53236363+breathinglogic@users.noreply.github.com>
2022-02-18 22:45:15 +0000

    Update create-vm-azure.ps1

--- powershell_script/create-vm-azure.ps1
+++ powershell_script/create-vm-azure.ps1
@@ -23,6 +23,7 @@ try {
         --name $VmName `
         --image win2016datacenter `
         --admin-password $Env:SP_CLIENT_SECRET `
+        --public-ip-sku Standard `
     }
 catch {
     Write-Output "VM already exists"
